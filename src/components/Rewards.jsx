aa8232dc93be5e069257e93f111dde9aded517c5
Harsh Tarkar <tarkar.harsh@gmail.com>
2024-09-03 01:21:37 +0530

    Hosting #18 - All images fixed

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -73,110 +73,110 @@ function Rewards(props){
                     {certSealCount>0 &&
                     <div name="cert-seal" className="reward">
                         <div>
-                            <img src="./src/assets/images/cert-seal-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/cert-seal-purple.png"}></img>
                             <p>: {reward[levelSelector].certSealPDown * certSealCount}-{reward[levelSelector].certSealPUp * certSealCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/cert-seal-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/cert-seal-blue.png"}></img>
                             <p>: {reward[levelSelector].certSealBDown * certSealCount}-{reward[levelSelector].certSealBUp * certSealCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/cert-seal-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/cert-seal-green.png"}></img>
                             <p>: {reward[levelSelector].certSealGDown * certSealCount}-{reward[levelSelector].certSealGUp * certSealCount}</p>
                         </div>
                     </div>}
                     {wEngineCount>0 &&
                     <div name="component" className="reward">
                         <div>
-                            <img src="./src/assets/images/component-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/component-purple.png"}></img>
                             <p>: {reward[levelSelector].wEnginePDown * wEngineCount}-{reward[levelSelector].wEnginePUp * wEngineCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/component-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/component-blue.png"}></img>
                             <p>: {reward[levelSelector].wEngineBDown * wEngineCount}-{reward[levelSelector].wEngineBUp * wEngineCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/component-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/component-green.png"}></img>
                             <p>: {reward[levelSelector].wEngineGDown * wEngineCount}-{reward[levelSelector].wEngineGUp * wEngineCount}</p>
                         </div>
                     </div>}
                     {skillChipCount>0 &&
                     <div name="skill-chip" className="reward">
                         <div>
-                            <img src="./src/assets/images/skill-chip-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/skill-chip-purple.png"}></img>
                             <p>: {reward[levelSelector].skillChipPDown * skillChipCount}-{reward[levelSelector].skillChipPUp * skillChipCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/skill-chip-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/skill-chip-blue.png"}></img>
                             <p>: {reward[levelSelector].skillChipBDown * skillChipCount}-{reward[levelSelector].skillChipBUp * skillChipCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/skill-chip-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/skill-chip-green.png"}></img>
                             <p>: {reward[levelSelector].skillChipGDown * skillChipCount}-{reward[levelSelector].skillChipGUp * skillChipCount}</p>
                         </div>
                     </div>}
                     {hdDataCount > 0 &&
                     <div name="h-d-d" className="reward">
                         <div>
-                            <img src="./src/assets/images/higher-dimensional-data-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/higher-dimensional-data-purple.png"}></img>
                             <p>: {reward[levelSelector].hdDataValue * hdDataCount}</p>
                         </div>
                     </div>}
                     {invLogCount > 0 &&
                     <div name="inv-log" className="reward">
                         <div>
-                            <img src="./src/assets/images/inv-log-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/inv-log-purple.png"}></img>
                             <p>: {reward[levelSelector].invLogPDown * invLogCount}-{reward[levelSelector].invLogPUp * invLogCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/inv-log-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/inv-log-blue.png"}></img>
                             <p>: {reward[levelSelector].invLogBDown * invLogCount}-{reward[levelSelector].invLogBUp * invLogCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/inv-log-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/inv-log-green.png"}></img>
                             <p>: {reward[levelSelector].invLogGDown * invLogCount}-{reward[levelSelector].invLogGUp * invLogCount}</p>
                         </div>
                     </div>}
                     {energyModuleCount > 0 &&
                     <div name="energy-module" className="reward">
                         <div>
-                            <img src="./src/assets/images/energy-module-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/energy-module-purple.png"}></img>
                             <p>: {reward[levelSelector].energyModulePDown * energyModuleCount}-{reward[levelSelector].energyModulePUp * energyModuleCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/energy-module-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/energy-module-blue.png"}></img>
                             <p>: {reward[levelSelector].energyModuleBDown * energyModuleCount}-{reward[levelSelector].energyModuleBUp * energyModuleCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/energy-module-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/energy-module-green.png"}></img>
                             <p>: {reward[levelSelector].energyModuleGDown * energyModuleCount}-{reward[levelSelector].energyModuleGUp * energyModuleCount}</p>
                         </div>
                     </div>}
                     {platingAgentCount >0 &&
                     <div name="plating-agent" className="reward">
                         <div>
-                            <img src="./src/assets/images/plating-agent-purple.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/plating-agent-purple.png"}></img>
                             <p>: {reward[levelSelector].platingAgentPDown * platingAgentCount}-{reward[levelSelector].platingAgentPUp * platingAgentCount}</p>
                         </div>
                         
                         <div>
-                            <img src="./src/assets/images/plating-agent-blue.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/plating-agent-blue.png"}></img>
                             <p>: {reward[levelSelector].platingAgentBDown * platingAgentCount}-{reward[levelSelector].platingAgentBUp * platingAgentCount}</p>
                         </div>
                         <div>
-                            <img src="./src/assets/images/plating-agent-green.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/plating-agent-green.png"}></img>
                             <p>: {reward[levelSelector].platingAgentGDown * platingAgentCount}-{reward[levelSelector].platingAgentGUp * platingAgentCount}</p>
                         </div>
                     </div>}
                     {dennyCount > 0 &&
                     <div name="denny" className="reward">
                         <div>
-                            <img src="./src/assets/images/denny.png"></img>
+                            <img src={import.meta.env.BASE_URL + "/images/denny.png"}></img>
                             <p>: {reward[levelSelector].dennys * dennyCount}</p>
                         </div>
                     </div>}
